75f06a92f84c81fed1e8d8bf1994d2c4d860324d
shutrup <magagaraev95@mail.ru>
2023-07-14 11:39:16 +0300

    Initial Commit

---
+++ RealTimeMessenger/RealTimeMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeMessengerApp.swift
+//  RealTimeMessenger
+//
+//  Created by Шарап Бамматов on 14.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RealTimeMessengerUITests/RealTimeMessengerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RealTimeMessengerUITestsLaunchTests.swift
+//  RealTimeMessengerUITests
+//
+//  Created by Шарап Бамматов on 14.07.2023.
+//
+
+import XCTest
+
+final class RealTimeMessengerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
